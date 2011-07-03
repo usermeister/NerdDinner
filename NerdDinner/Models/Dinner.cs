7986826ab4576035ffd865ba9b3ca00d63e2c85d
Usermeister <usermeister1@gmail.com>
2011-07-04 00:42:43 +0200

    Adding state at page 31.

---
+++ NerdDinner/Models/Dinner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace NerdDinner.Models
+{
+    [MetadataType(typeof(Dinner_Validation))]
+    public partial class Dinner
+    {
+        //...
+    }
+    public class Dinner_Validation
+    {
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(50, ErrorMessage = "Title may not be longer than 50 characters")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(256, ErrorMessage =
+          "Description must be 256 characters or less")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Country is required")]
+        public string Country { get; set; }
+
+        [Required(ErrorMessage = "Phone# is required")]
+        public string ContactPhone { get; set; }
+    }
+}
